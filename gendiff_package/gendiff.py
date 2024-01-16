1af7e4c754a43fe02eda7580875b75791c01dc6a
macbook <ramisphoto>
2024-01-16 17:01:33 +0300

    fix lint

--- gendiff_package/gendiff.py
+++ gendiff_package/gendiff.py
@@ -13,10 +13,10 @@ def bool_to_str(dict):
 
 
 def generate_diff(path_to_file1, path_to_file2):
-    with open(path_to_file1, "r") as file1:         #json.load(open(path_to_file1))
-        with open(path_to_file2, "r") as file2:     
-            data_file1 = json.load(file1) #dict
-            data_file2 = json.load(file2) #dict
+    with open(path_to_file1, "r") as file1:
+        with open(path_to_file2, "r") as file2:
+            data_file1 = json.load(file1)  # dict
+            data_file2 = json.load(file2)  # dict
             diff = {}
             for key1 in sorted(data_file1):
                 if key1 in data_file2:
@@ -31,8 +31,6 @@ def generate_diff(path_to_file1, path_to_file2):
                 if key2 not in data_file1:
                     diff['  + ' + key2] = data_file2[key2]
             diff = bool_to_str(diff)
-            return("{\n" + 
-                   "\n".join([f'{key}: {value}' for key, value in diff.items()])
-                   + "\n}")
-
-#print(generate_diff("gendiff_package/scripts/fixtures/file1.json", "gendiff_package/scripts/fixtures/file2.json"))
+            return ("{\n"
+                    + "\n".join([f'{key}: {value}' for key, value in diff.items()])
+                    + "\n}")
--- gendiff_package/scripts/gendiff.py
+++ gendiff_package/scripts/gendiff.py
@@ -4,11 +4,11 @@
 from gendiff_package.parsing import parsing
 from gendiff_package.gendiff import generate_diff
 
+
 def main():
     file1, file2 = parsing()
     print(generate_diff(file1, file2))
-    
-    
+
+
 if __name__ == '__main__':
     main()
-
