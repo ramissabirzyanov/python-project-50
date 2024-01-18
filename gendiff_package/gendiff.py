6de8130110530b25c52c5a28d364c4b45436e7a7
macbook <ramisphoto>
2024-01-18 15:39:45 +0300

    update

--- gendiff_package/gendiff.py
+++ gendiff_package/gendiff.py
@@ -12,9 +12,7 @@ def bool_to_str(dict):
     return dict
 
 
-def generate_diff(path_to_file1, path_to_file2):
-    data_file1 = read_file(path_to_file1)  # dict
-    data_file2 = read_file(path_to_file2)  # dict
+def make_diff(data_file1, data_file2):
     diff = {}
     for key1 in sorted(data_file1):
         if key1 in data_file2:
@@ -28,6 +26,13 @@ def generate_diff(path_to_file1, path_to_file2):
     for key2 in sorted(data_file2):
         if key2 not in data_file1:
             diff['  + ' + key2] = data_file2[key2]
+    return diff
+
+
+def generate_diff(path_to_file1, path_to_file2):
+    data_file1 = read_file(path_to_file1)  # dict
+    data_file2 = read_file(path_to_file2)  # dict
+    diff = make_diff(data_file1, data_file2)
     diff = bool_to_str(diff)
     return ("{\n"
             + "\n".join([f'{key}: {value}' for key, value in diff.items()])
