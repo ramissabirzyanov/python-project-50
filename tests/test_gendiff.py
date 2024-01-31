13f5309b88b93b26cfc5892ef40ae0c793b6b1f3
macbook <ramisphoto>
2024-01-31 16:15:48 +0300

    fix tests

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -2,21 +2,21 @@ from gendiff_package.gendiff import generate_diff
 
 
 def test_gendiff_json():
-    diff = generate_diff("tests/fixtures/file1.json", "tests/fixtures/file2.json")
+    diff = generate_diff("tests/fixtures/file1.json", "tests/fixtures/file2.json", format='stylish')
     expected = open("tests/fixtures/expected_plain.txt").read()
     assert diff == expected
 
 def test_gendiff_yaml():
-    diff = generate_diff("tests/fixtures/file1.yaml", "tests/fixtures/file2.yaml")
+    diff = generate_diff("tests/fixtures/file1.yaml", "tests/fixtures/file2.yaml", format='stylish')
     expected = open("tests/fixtures/expected_plain.txt").read()
     assert diff == expected
 
 def test_gendiff_tree_json():
-    diff = generate_diff("tests/fixtures/tree1.json", "tests/fixtures/tree2.json")
+    diff = generate_diff("tests/fixtures/tree1.json", "tests/fixtures/tree2.json", format='stylish')
     expected = open("tests/fixtures/expected_tree.txt").read()
     assert diff == expected
 
 def test_gendiff_tree_yaml():
-    diff = generate_diff("tests/fixtures/tree1.yaml", "tests/fixtures/tree2.yaml")
+    diff = generate_diff("tests/fixtures/tree1.yaml", "tests/fixtures/tree2.yaml", format='stylish')
     expected = open("tests/fixtures/expected_tree.txt").read()
     assert diff == expected
