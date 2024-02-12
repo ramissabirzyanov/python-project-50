5480f99ddcfb7a61969d678cd30095497256d0a0
macbook <ramisphoto>
2024-02-12 15:59:39 +0300

    fix lint

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -2,12 +2,11 @@ import pytest
 from gendiff import generate_diff
 
 
-@pytest.fixture(
-        params=[
-        ('stylish', open("tests/fixtures/expected_stylish.txt").read()),
-        ('plain', open("tests/fixtures/expected_plain.txt").read()),
-        ('json', open("tests/fixtures/expected_json.txt").read()),
-        ])
+@pytest.fixture(params=
+        [('stylish', open("tests/fixtures/expected_stylish.txt").read()),
+         ('plain', open("tests/fixtures/expected_plain.txt").read()),
+         ('json', open("tests/fixtures/expected_json.txt").read())]
+        )
 def formats(request):
     return request.param
 
