ae32889ddb9105a4958cac69fa396141ea2e98c2
macbook <ramisphoto>
2024-02-12 16:05:07 +0300

    fix lint

--- tests/test_parsing.py
+++ tests/test_parsing.py
@@ -4,10 +4,9 @@ from gendiff.parsing import read_file
 def test_read_file():
     file = read_file("tests/fixtures/file1.json")
     expected = {
-    "host": "hexlet.io",
-    "timeout": 50,
-    "proxy": "123.234.53.22",
-    "follow": False
-    }
+                "host": "hexlet.io",
+                "timeout": 50,
+                "proxy": "123.234.53.22",
+                "follow": False
+                }
     assert file == expected
-    
