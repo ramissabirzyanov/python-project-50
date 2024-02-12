3e30043edd3eb6f8a3017911994eca4688cc2b29
macbook <ramisphoto>
2024-02-12 10:27:04 +0300

    update tests

--- tests/test_gendiff_json.py
+++ tests/test_gendiff_json.py
@@ -2,6 +2,6 @@ from gendiff import generate_diff
 
 
 def test_gendiff_tree_json():
-    diff = generate_diff("tests/fixtures/tree1.json", "tests/fixtures/tree2.json", format='json')
+    diff = generate_diff("tests/fixtures/tree1.json", "tests/fixtures/tree2.json", 'json')
     expected = open("tests/fixtures/expected_json.txt").read()
     assert diff == expected
--- tests/test_gendiff_plain.py
+++ tests/test_gendiff_plain.py
@@ -1,11 +1,11 @@
 from gendiff import generate_diff
 
 def test_gendiff_tree_json():
-    diff = generate_diff("tests/fixtures/tree1.json", "tests/fixtures/tree2.json", format='plain')
+    diff = generate_diff("tests/fixtures/tree1.json", "tests/fixtures/tree2.json", 'plain')
     expected = open("tests/fixtures/expected_plain.txt").read()
     assert diff == expected
 
 def test_gendiff_tree_yaml():
-    diff = generate_diff("tests/fixtures/tree1.yaml", "tests/fixtures/tree2.yaml", format='plain')
+    diff = generate_diff("tests/fixtures/tree1.yaml", "tests/fixtures/tree2.yaml", 'plain')
     expected = open("tests/fixtures/expected_plain.txt").read()
     assert diff == expected
