cacdcab22991dff8c447084da4bde39942f953ce
macbook <ramisphoto>
2024-02-12 15:56:19 +0300

    fix lint

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,16 +1,16 @@
 import pytest
-import os
 from gendiff import generate_diff
 
 
-@pytest.fixture(params=[
-    ('stylish', open("tests/fixtures/expected_stylish.txt").read()),
-    ('plain', open("tests/fixtures/expected_plain.txt").read()),
-    ('json', open("tests/fixtures/expected_json.txt").read()),
-    ])
+@pytest.fixture(
+        params=[
+        ('stylish', open("tests/fixtures/expected_stylish.txt").read()),
+        ('plain', open("tests/fixtures/expected_plain.txt").read()),
+        ('json', open("tests/fixtures/expected_json.txt").read()),
+        ])
 def formats(request):
     return request.param
-    
+
 
 @pytest.fixture
 def get_path():
