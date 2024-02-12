ac04c7fa14a7e04684c825f7be28d215a0927966
macbook <ramisphoto>
2024-02-12 14:40:46 +0300

    upgrade tests

---
+++ tests/test_gendiff.py
@@ -0,0 +1,21 @@
+import pytest
+from gendiff import generate_diff
+
+
+@pytest.fixture(scope='function', params=[
+    ('stylish', open("tests/fixtures/expected_stylish.txt").read()),
+    ('plain', open("tests/fixtures/expected_plain.txt").read()),
+    ('json', open("tests/fixtures/expected_json.txt").read()),
+    ])
+def formats(request):
+    return request.param
+    
+
+@pytest.fixture
+def paths_of_trees():
+    return ("tests/fixtures/tree1.json", "tests/fixtures/tree2.json")
+
+
+def test_gendiff(paths_of_trees, formats):
+    format, expected = formats
+    assert generate_diff(paths_of_trees[0], paths_of_trees[1], format) == expected
